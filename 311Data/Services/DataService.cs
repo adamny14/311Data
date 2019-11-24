48923824012fab520ba474f4a96ffe3d0000687e
Adam Hussain <adamahussain528@gmail.com>
2019-11-24 14:29:52 -0500

    Added files to create Mongodb connection

---
+++ 311Data/Services/DataService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _311Data.Models;
+using MongoDB.Driver;
+
+namespace _311Data.Services
+{
+    public class DataService
+    {
+        private readonly IMongoCollection<IssueData> _issueData;
+
+        public DataService(IDataDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _issueData = database.GetCollection<IssueData>(settings.DataCollectionName);
+        }
+    }
+}
--- 311Data/Startup.cs
+++ 311Data/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _311Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace _311Data
 {
@@ -25,6 +27,12 @@ namespace _311Data
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DataDatabaseSettings>(
+                Configuration.GetSection(nameof(DataDatabaseSettings)));
+
+            services.AddSingleton<IDataDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions < DataDatabaseSettings >> ().Value);
+
             services.AddControllers();
         }
 
